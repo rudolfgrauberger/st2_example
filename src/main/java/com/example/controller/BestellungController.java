445c376acce6b37b4fe39d839298e8bc9184055e
Pierre Wilken <pierre_w@live.com>
2018-06-10 20:17:15 +0200

    change api root names to plural

--- src/main/java/com/example/controller/BestellungController.java
+++ src/main/java/com/example/controller/BestellungController.java
@@ -22,61 +22,61 @@ public class BestellungController {
 
     // A1
     // OK
-    @PostMapping("/bestellung")
+    @PostMapping("/bestellungen")
     @ResponseBody
     @ResponseStatus(value = HttpStatus.CREATED)
         public Bestellung bestellung(@RequestBody DateInput datum) {
-        System.out.println("Post -> /bestellung | Datum: "+datum.getDatum());
+        System.out.println("Post -> /bestellungen | Datum: "+datum.getDatum());
         Bestellung bestellung = new Bestellung(datum.getDatum());
         bestellungService.saveBestellung(bestellung);
         return bestellung;
     }
     // OK
-    @GetMapping("/bestellung")
+    @GetMapping("/bestellungen")
     @ResponseBody
     public List<Bestellung> getBestellungen() {
-        System.out.println("Get -> /bestellung");
+        System.out.println("Get -> /bestellungen");
         return bestellungService.getAll();
     }
 
     // A3, A5, A6
     // OK
-    @GetMapping("/bestellung/{ordernummer}")
+    @GetMapping("/bestellungen/{ordernummer}")
     @ResponseBody
     public Bestellung getBestellungOrdernummer(@PathVariable String ordernummer) {
         Bestellung bestellung = bestellungService.findBestellungByOrdernummer(ordernummer);
 
         if(bestellung == null) {
-            System.out.println("Get -> /bestellung | not found");
+            System.out.println("Get -> /bestellungen | not found");
         } else {
-            System.out.println("Get -> /bestellung/" + bestellung.getOrdernummer());
+            System.out.println("Get -> /bestellungen/" + bestellung.getOrdernummer());
         }
 
         return bestellung;
     }
 
 
-    @DeleteMapping("/bestellung/{ordernummer}")
+    @DeleteMapping("/bestellungen/{ordernummer}")
     // OK
     public @ResponseBody String deleteBestellungOrdernummer(@PathVariable String ordernummer) {
         boolean deleted = bestellungService.deleteBestellung(ordernummer);
-        System.out.println("Delete -> /bestellung/"+ordernummer +" | Deleted: "+ deleted);
-        return "/bestellung";
+        System.out.println("Delete -> /bestellungen/"+ordernummer +" | Deleted: "+ deleted);
+        return "/bestellungen";
     }
 
     // OK
-    @PutMapping("/bestellung/{ordernummer}")
+    @PutMapping("/bestellungen/{ordernummer}")
     public String putBestellungOrdernummer(@PathVariable String ordernummer, @RequestBody DateInput datum) {
         boolean changed = bestellungService.changeDate(ordernummer, datum);
-        System.out.println("Put -> /bestellung/{ordernummer} | Changed: " +changed);
+        System.out.println("Put -> /bestellungen/{ordernummer} | Changed: " +changed);
         // ToDo: Right Response
         return "/"+ordernummer;
     }
 
     // A2, A4 // ToDo: wird nicht aufgerufen, statt dessen /bestellung
-    @RequestMapping(value="/bestellung?operation= greaterthan&datum={datum}", method = RequestMethod.GET)
+    @RequestMapping(value="/bestellungen?operation=greaterthan&datum={datum}", method = RequestMethod.GET)
     public List<Bestellung> getBestellungOrdernummerGreaterThanDate(@PathVariable Date datum) {
-        System.out.println("Get -> /bestellung?operation= greaterthan&datum={datum}");
+        System.out.println("Get -> /bestellungen?operation= greaterthan&datum={datum}");
         List<Bestellung> bestellungList = bestellungService.greaterThanDatum(datum);
         return bestellungList;
     }
