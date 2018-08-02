f0dc01c7a13247c27ad2e6a402a8100a40020bcb
Rudolf Grauberger <rudolf@grauberger.org>
2018-08-03 01:51:57 +0200

    Fixes accidentally added file

--- src/test/java/com/example/ExampleApplicationTests.java
+++ src/test/java/com/example/ExampleApplicationTests.java
@@ -1,55 +1,16 @@
 package com.example;
 
-import com.example.Entities.Author;
-import com.example.Entities.Book;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class ExampleApplicationTests {
 
 	@Test
-	public void testSerializeAuthorWithCircularDependenciesToJson() throws ParseException, JsonProcessingException {
-		String testDate = "29-10-2010";
-		DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-		Date date = formatter.parse(testDate);
-
-		Author author1 = new Author("NoName Author");
-		Book book1 = new Book("My Book", date);
-		author1.addBook(book1);
-
-
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(author1);
-
-
-		assertThat(json).isEqualTo("{\"name\":\"NoName Author\",\"books\":[{\"name\":\"My Book\",\"publicationDate\":1288303200000}]}");
+	public void contextLoads() {
 	}
 
-	@Test
-	public void testSerializeBookWithCircularDependenciesToJson() throws ParseException, JsonProcessingException {
-		String testDate = "29-10-2010";
-		DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-		Date date = formatter.parse(testDate);
-
-		Author author1 = new Author("NoName Author");
-		Book book1 = new Book("My Book", date);
-		author1.addBook(book1);
-		String json = null;
-
-		ObjectMapper mapper = new ObjectMapper();
-		json = mapper.writeValueAsString(book1);
-
-		assertThat(json).isEqualTo("{\"name\":\"My Book\",\"publicationDate\":1288303200000,\"authors\":[{\"name\":\"NoName Author\"}]}");
-	}
 }
